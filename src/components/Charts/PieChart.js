99bc04d22ae92c69e4ab1b1e05a1eb45f79194e4
gonzalo10 <gonzalo.achiaga@gmail.com>
2019-10-09 22:48:10 +0200

    first test to pass travis done

--- src/components/Charts/PieChart.js
+++ src/components/Charts/PieChart.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 
 import { PieChart as PieChartBase, Pie, Sector, Cell } from 'recharts';
 
@@ -8,59 +7,58 @@ const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 const RADIAN = Math.PI / 180;
 
 export const PieChart = ({ data }) => {
-  const [chartValues, setChartValues] = useState();
+	const [chartValues, setChartValues] = useState();
 
-  useEffect(() => {
-    console.log(chartValues, data);
-    if (data) {
-      const values = [];
-      Object.keys(data).forEach(item => {
-        values.push({ name: item, value: data[item] });
-      });
-      setChartValues(values);
-    }
-  }, [data]);
+	useEffect(() => {
+		if (data) {
+			const values = [];
+			Object.keys(data).forEach(item => {
+				values.push({ name: item, value: data[item] });
+			});
+			setChartValues(values);
+		}
+	}, [data]);
 
-  const renderCustomizedLabel = ({
-    cx,
-    cy,
-    midAngle,
-    innerRadius,
-    outerRadius,
-    percent,
-    index,
-  }) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
-    return (
-      <text
-        x={x}
-        y={y}
-        fill='white'
-        textAnchor={x > cx ? 'start' : 'end'}
-        dominantBaseline='central'>
-        {percent * 100 > 0 && `${(percent * 100).toFixed(0)}%`}
-      </text>
-    );
-  };
+	const renderCustomizedLabel = ({
+		cx,
+		cy,
+		midAngle,
+		innerRadius,
+		outerRadius,
+		percent,
+		index,
+	}) => {
+		const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+		const x = cx + radius * Math.cos(-midAngle * RADIAN);
+		const y = cy + radius * Math.sin(-midAngle * RADIAN);
+		return (
+			<text
+				x={x}
+				y={y}
+				fill='white'
+				textAnchor={x > cx ? 'start' : 'end'}
+				dominantBaseline='central'>
+				{percent * 100 > 0 && `${(percent * 100).toFixed(0)}%`}
+			</text>
+		);
+	};
 
-  if (!chartValues) return null;
-  return (
-    <PieChartBase width={200} height={200}>
-      <Pie
-        data={chartValues}
-        cx={100}
-        cy={100}
-        labelLine={false}
-        label={renderCustomizedLabel}
-        outerRadius={80}
-        fill='#8884d8'
-        dataKey='value'>
-        {chartValues.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-    </PieChartBase>
-  );
+	if (!chartValues) return null;
+	return (
+		<PieChartBase width={200} height={200}>
+			<Pie
+				data={chartValues}
+				cx={100}
+				cy={100}
+				labelLine={false}
+				label={renderCustomizedLabel}
+				outerRadius={80}
+				fill='#8884d8'
+				dataKey='value'>
+				{chartValues.map((entry, index) => (
+					<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+				))}
+			</Pie>
+		</PieChartBase>
+	);
 };
