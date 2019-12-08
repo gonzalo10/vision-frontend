d0fe5522388deacb490c220d7a8934e82a6304c2
gonzalo10 <gonzalo.achiaga@gmail.com>
2019-12-08 18:57:28 +0100

    feat: changed backendURL value

--- src/_services/helpers.js
+++ src/_services/helpers.js
@@ -1,27 +1,28 @@
-import { authHeader } from "../helpers";
-import { userActions } from "../_actions";
-import { store } from "../helpers";
+import { authHeader } from '../helpers';
+import { userActions } from '../_actions';
+import { store } from '../helpers';
 
-export const backendURL = process.env.REACT_APP_BACKEND_URL;
+export const backendURL =
+	'http://vison-app-backend.us-east-1.elasticbeanstalk.com';
 
 export const API = requestBody =>
-  fetch(backendURL + "/graphql", {
-    method: "POST",
-    body: JSON.stringify(requestBody),
-    headers: authHeader()
-  }).then(response => handleResponse(response));
+	fetch(backendURL + '/graphql', {
+		method: 'POST',
+		body: JSON.stringify(requestBody),
+		headers: authHeader(),
+	}).then(response => handleResponse(response));
 
 function handleResponse(response) {
-  return response.text().then(text => {
-    if (!response.ok) {
-      if (response.status === 401) {
-        store.dispatch(userActions.logout());
-        window.location.reload(true);
-      }
-      const error = response.statusText;
-      return Promise.reject(error);
-    }
-    const data = text && JSON.parse(text);
-    return data.data;
-  });
+	return response.text().then(text => {
+		if (!response.ok) {
+			if (response.status === 401) {
+				store.dispatch(userActions.logout());
+				window.location.reload(true);
+			}
+			const error = response.statusText;
+			return Promise.reject(error);
+		}
+		const data = text && JSON.parse(text);
+		return data.data;
+	});
 }
